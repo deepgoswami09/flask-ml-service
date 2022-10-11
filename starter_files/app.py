bea21835f200126521ba5c8b4db7b6fe7cbdcf2e
deepgoswami09 <115372822+deepgoswami09@users.noreply.github.com>
2022-10-11 18:52:06 +0530

    Update app.py

--- starter_files/app.py
+++ starter_files/app.py
@@ -57,7 +57,7 @@ def predict():
 
     try:
         clf = joblib.load("boston_housing_prediction.joblib")
-    except:
+    except Exception as e:
         LOG.info("JSON payload: %s json_payload")
         print(e)
         traceback.format_exc()
