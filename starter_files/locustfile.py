1c6ee5c30d1cb064c3ab4ba9edcc3070509dd935
deepgoswami09 <115372822+deepgoswami09@users.noreply.github.com>
2022-10-11 19:44:01 +0530

    Update locustfile.py

--- starter_files/locustfile.py
+++ starter_files/locustfile.py
@@ -3,5 +3,5 @@ from locust import HttpUser, task
 class User(HttpUser):
     @task
     def mainPage(self):
-        self.client.get("http://localhost:5000/")
-        self.client.post("http://localhost:5000/predict")
+        self.client.get("https://subhadeepapp11.azurewebsites.net/")
+        self.client.post("https://subhadeepapp11.azurewebsites.net:443/predict")
