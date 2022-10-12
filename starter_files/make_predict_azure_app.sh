cd35b1b44dc9a9fce1fc00e76a103deff47267c9
deepgoswami09 <115372822+deepgoswami09@users.noreply.github.com>
2022-10-12 23:40:25 +0530

    Update make_predict_azure_app.sh

--- starter_files/make_predict_azure_app.sh
+++ starter_files/make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://subhadeepawebpp11.azurewebsites.net:$PORT/predict 
+     -X POST https://subhadeepwebapp11.azurewebsites.net:$PORT/predict 
      # TODO: Replace the <yourappname> with your application name 
